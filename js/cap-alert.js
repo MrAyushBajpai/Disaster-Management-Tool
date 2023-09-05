1adacdd6192bed1df3b0f5657d48cdc579c838f0
MrAyushBajpai <ayushbajpai1711@gmail.com>
2023-09-05 12:27:24 +0530

    Removed cap-alert color changer

--- js/cap-alert.js
+++
@@ -1,8 +0,0 @@
-const capAlertCard = document.getElementById("cap-alert-card");
-
-function changeBackgroundColorToRed() {
-    capAlertCard.style.backgroundColor = "#008000";
-    capAlertCard.style.color = "#fff";
-}
-
-changeBackgroundColorToRed();
