440743e22e47ce54cd390b1dec705f618e948d33
MrAyushBajpai <ayushbajpai1711@gmail.com>
2023-09-05 12:21:32 +0530

    Updated Theme

--- js/cap-alert.js
+++ js/cap-alert.js
@@ -2,6 +2,7 @@ const capAlertCard = document.getElementById("cap-alert-card");
 
 function changeBackgroundColorToRed() {
     capAlertCard.style.backgroundColor = "#008000";
+    capAlertCard.style.color = "#fff";
 }
 
 changeBackgroundColorToRed();
