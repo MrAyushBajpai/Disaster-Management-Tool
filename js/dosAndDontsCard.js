888a3953ea9ed69eec63ae42295f408479c81ba5
MrAyushBajpai <ayushbajpai1711@gmail.com>
2023-09-06 19:40:01 +0530

    Added Dos and Donts List

---
+++ js/dosAndDontsCard.js
@@ -0,0 +1,6 @@
+const card = document.getElementById("dosAndDontsCard")
+// Attach an onclick event listener
+card.addEventListener("click", function() {
+    // Navigate to dos-and-donts.html when the card is clicked
+    window.location.href = "dos-and-donts.html";
+});
