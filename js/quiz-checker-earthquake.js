c74bd92bbc44f821fef4c74009517365fa61f489
MrAyushBajpai <ayushbajpai1711@gmail.com>
2023-09-09 18:28:36 +0530

    Added Quiz Checker for Earthquake Mock Exercise

---
+++ js/quiz-checker-earthquake.js
@@ -0,0 +1,33 @@
+const correctAnswers = {
+    q1: 'c',
+    q2: 'a',
+    q3: 'b',
+    q4: 'c',
+    q5: 'b',
+    q6: 'a',
+    q7: 'b',
+    q8: 'c',
+    q9: 'c',
+    q10: 'b',
+};
+
+// Function to calculate and display the results
+function calculateResults() {
+    const form = document.getElementById('quiz-form');
+    let score = 0;
+
+    // Loop through each question and check the selected answer
+    for (let i = 1; i <= 10; i++) {
+        const selectedAnswer = form.elements['q' + i].value;
+        if (selectedAnswer === correctAnswers['q' + i]) {
+            score++;
+        }
+    }
+
+    // Display the score
+    alert('Your score: ' + score + ' out of 10');
+}
+
+// Attach an event listener to the submit button
+const submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click', calculateResults);
